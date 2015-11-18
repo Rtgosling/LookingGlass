930105036c23dd5445f84cd02e6ca7e50fe4a1ef
Rtgosling <ryan.gosling@students.plymouth.ac.uk>
2015-11-18 17:03:18 +0000

    Front End
    Added folder structure for front end, with initial scripts being used

---
+++ front_end/Scripts/CardPop.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.UI;// for ui changing
+using System.Collections;
+using System.Collections.Generic; //needed for list
+
+
+public class CardPop : MonoBehaviour {
+
+    //In inspector, drag sprite images into each variable
+    public Sprite cardback; // default image, is loaded and set in inspector
+    public Sprite[] faces; //Images are dragged into inspector
+    public int number;
+
+    public List<int> cards;
+
+
+    Image myImageComponent;
+
+    //Need to be able to reference the hand image objects and assign images to them. 
+
+    void Start()
+    {
+
+        myImageComponent = GetComponent<Image>(); //This could be canvasrenderer, image, etc. Gets a part of an object that you can see in inspector
+        
+    }
+
+    //On button press choose a random card and display it. Effectively a random card is scanned and then displayed. The button acts as the scan.
+    public void cardPopup () { //must be public to be accessed outside. When button is pressed, activate this function
+
+        number = Random.Range(0, 9); // generate random number between 0 and the size of the deck (-1) to choose a random card
+        myImageComponent.sprite = faces[number];
+
+        cards.Add(number); //Add index to list to save cards
+    }
+}
---
+++ front_end/Scripts/ShowHand.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(CardPop))] //requires cardpop to work
+
+public class ShowHand : MonoBehaviour {
+    public Sprite[] faces; //Images are dragged into inspector, must be the same order as in cardpop
+
+    public Vector3 start;
+    public float cardOffset;
+
+    public GameObject cardPrefab;
+    CardPop cardPop;
+
+    int cardCount = 0; //to save how many cards in handss
+
+    void Start()
+    {
+        cardPop = GetComponent<CardPop>();
+        
+
+    }
+
+    public void ShowCards(){
+    
+        float co = cardOffset * cardCount; //for calculating the position of the next card
+
+        GameObject cardCopy = (GameObject)Instantiate(cardPrefab); //takes in cardPrefab and creates a copy of it
+        Vector3 temp = start + new Vector3(co, 0f); 
+        cardCopy.transform.position = temp;  //positioning each card after the last
+
+        float myScale = 0.8f;
+        cardCopy.transform.localScale = new Vector3(myScale, myScale); //resize the clones 
+
+        cardCopy.GetComponent<SpriteRenderer>().sprite = faces[cardPop.number]; //
+
+        cardCount++;
+        
+    }
+}
